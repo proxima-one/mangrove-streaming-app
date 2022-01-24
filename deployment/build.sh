c49abef83c9d3d120794bf1f3abe50075a79e9d8
ivanbenko <ivan@krafteq.de>
2022-01-24 23:28:17 +0100

    fix docker runtime

--- deployment/build.sh
+++ deployment/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version=${$1:-dev}
+version=${1:-dev}
 base=$(dirname "$0")
 
 DOCKER_BUILDKIT=1 docker build --build-arg BUILD_VERSION=$version --tag proximaone/streaming-app:mangrove-$version --file $base/Dockerfile --target prod --progress=plain --secret id=npmrc,src=$HOME/.npmrc $base/..
