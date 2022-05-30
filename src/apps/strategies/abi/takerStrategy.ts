3f5363f6af8315a3c73316281d8ac5275ca3387d
Alexei Savostin <>
2022-05-30 14:46:24 +0200

    Mangrove strategies stream

---
+++ src/apps/strategies/abi/takerStrategy.ts
@@ -0,0 +1,57 @@
+export const takerStrategy = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "base",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "quote",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "taker",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "bool",
+        name: "selling",
+        type: "bool",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "takerGot",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "takerGave",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "penalty",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "restingOrderId",
+        type: "uint256",
+      },
+    ],
+    name: "OrderSummary",
+    type: "event",
+  },
+];
---
+++ src/apps/strategies/strategiesParser.ts
@@ -0,0 +1,180 @@
+import * as proxima from "@proxima-one/proxima-core";
+import * as ethersAbi from "@ethersproject/abi";
+import * as mangrove from "@proximaone/stream-schema-mangrove";
+import _ from "lodash";
+import { takerStrategy } from "./abi/takerStrategy";
+import { multiUserStrategy } from "./abi/multiUserStrategy";
+
+const multiUserStrategyInterface = new ethersAbi.Interface(multiUserStrategy);
+const takerStrategyInterface = new ethersAbi.Interface(takerStrategy);
+
+type OutputEvent =
+  | mangrove.strategyEvents.MultiUserStrategyEvent
+  | mangrove.strategyEvents.TakerStrategyEvent;
+
+type StrategyContractType = "multiUserStrategy" | "takerStrategy";
+
+export const MangroveStrategiesApp = proxima.eth.parseContractLogsApp({
+  contracts: {
+    multiUserStrategy: proxima.eth.ContractMetadata.fromAbi(multiUserStrategy),
+    takerStrategy: proxima.eth.ContractMetadata.fromAbi(takerStrategy),
+  },
+  discover: {
+    full: true,
+    event: ({ event, contractType }) => {
+      const address = event.payload.address;
+
+      if (
+        contractType == "multiUserStrategy" &&
+        looksLikeTargetEvent(event, multiUserStrategyInterface)
+      ) {
+        return {
+          multiUserStrategy: [address],
+        };
+      }
+
+      if (
+        contractType == "takerStrategy" &&
+        looksLikeTargetEvent(event, takerStrategyInterface)
+      ) {
+        return {
+          takerStrategy: [address],
+        };
+      }
+
+      return {};
+    },
+  },
+  map: {
+    event: ({ event, tx, block, contractType, args }) => {
+      const chain = args.network;
+
+      const txRef = {
+        chain: chain,
+        blockNumber: block.header.number,
+        blockHash: block.header.hash.toHexString(),
+        txHash: tx.original.data.hash.toHexString(),
+        sender: tx.original.data.from.toHexString(),
+      };
+
+      const stream = outputStream(args, contractType);
+
+      const mappedEvent =
+        contractType == "multiUserStrategy"
+          ? mapMultiUserStrategyEvent(event)
+          : mapTakerStrategyEvent(event);
+
+      if (!mappedEvent) return [];
+
+      const outputEvent = _.assign(mappedEvent, {
+        tx: txRef,
+        id: id(chain, event.payload.address.toHexString(), event.index),
+        address: event.payload.address.toHexString(),
+      }) as OutputEvent;
+
+      return [{ output: outputEvent, stream: stream }];
+    },
+  },
+});
+
+function mapMultiUserStrategyEvent(event: proxima.eth.DecodedContractEvent) {
+  switch (event.payload.name) {
+    case "CreditMgvUser":
+      return {
+        type: "CreditMgvUser",
+
+        user: event.payload.requireParam("user").asString(),
+        amount: event.payload.requireParam("amount").asString(),
+      };
+    case "CreditUserTokenBalance":
+      return {
+        type: "CreditUserTokenBalance",
+
+        user: event.payload.requireParam("user").asString(),
+        token: event.payload.requireParam("token").asString(),
+        amount: event.payload.requireParam("amount").asString(),
+      };
+    case "DebitMgvUser":
+      return {
+        type: "DebitMgvUser",
+
+        user: event.payload.requireParam("user").asString(),
+        amount: event.payload.requireParam("amount").asString(),
+      };
+    case "DebitUserTokenBalance":
+      return {
+        type: "DebitUserTokenBalance",
+
+        user: event.payload.requireParam("user").asString(),
+        token: event.payload.requireParam("token").asString(),
+        amount: event.payload.requireParam("amount").asString(),
+      };
+    case "NewOwnedOffer":
+      return {
+        type: "NewOwnedOffer",
+
+        outboundToken: event.payload.requireParam("outbound_tkn").asString(),
+        inboundToken: event.payload.requireParam("inbound_tkn").asString(),
+        offerId: event.payload.requireParam("offerId").asString(),
+        owner: event.payload.requireParam("owner").asString(),
+      };
+    default:
+      return undefined;
+  }
+}
+
+function mapTakerStrategyEvent(event: proxima.eth.DecodedContractEvent) {
+  switch (event.payload.name) {
+    case "OrderSummary":
+      return {
+        type: "OrderSummary",
+
+        base: event.payload.requireParam("base").asString(),
+        quote: event.payload.requireParam("quote").asString(),
+        taker: event.payload.requireParam("taker").asString(),
+        selling: event.payload.requireParam("selling").asBool(),
+
+        takerGot: event.payload.requireParam("takerGot").asString(),
+        takerGave: event.payload.requireParam("takerGave").asString(),
+        penalty: event.payload.requireParam("penalty").asString(),
+        restingOrderId: event.payload.requireParam("restingOrderId").asString(),
+      };
+    default:
+      return undefined;
+  }
+}
+
+function id(chain: string, address: string, index: number): string {
+  return `${chain}-${address}-${index}`;
+}
+
+function looksLikeTargetEvent(
+  event: proxima.eth.DecodedContractEvent,
+  Interface: ethersAbi.Interface
+): boolean {
+  return Object.values(Interface.events).some((eventSchema) => {
+    return (
+      event.payload.name == eventSchema.name &&
+      event.payload.params.length == eventSchema.inputs.length &&
+      event.payload.params.every(
+        (x, index) => x.name == eventSchema.inputs[index].name
+      )
+    );
+  });
+}
+
+function outputStream(
+  args: proxima.eth.ParseContractLogsAppArgs<StrategyContractType>,
+  contractType: StrategyContractType
+) {
+  if (typeof args.outputStream == "string")
+    throw new Error(
+      "Invalid outputStream type. Use Record to specify output stream for each strategy"
+    );
+
+  const stream = args.outputStream![contractType];
+  if (!stream)
+    throw new Error(`OutputStream for ${contractType} is not specified`);
+
+  return stream;
+}
